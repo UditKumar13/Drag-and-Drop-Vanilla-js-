56b1596edc948c4baf5494b5a410367e43ea4e2d
Uditkumar13 <uditkumar2401@gmail.com>
2021-05-24 12:09:55 +0530

    making the invisible and hold work

--- js/drag and drop.js
+++ js/drag and drop.js
@@ -1,1 +1,25 @@
 console.log("drag and drop ");
+
+const fill = document.querySelector('.fill');
+
+const empties = document.querySelectorAll('.empty');
+// it will have multiple node list as we have 5 empty classes
+
+// fill listener 
+fill.addEventListener('dragstart',dragStart);
+fill.addEventListener('dragend' ,dragEnd);
+
+// Drag functions
+
+function dragStart(){
+console.log('dragstart');
+this.className += ' hold'; // when u append , there is a space see hold 
+// we want when we drag , our fill class which is our image should get invisible;
+setTimeout(()=>( this.className = 'invisible'), 0);
+
+}
+
+function dragEnd(){
+console.log('dragEnd');
+this.className = 'fill';
+}
